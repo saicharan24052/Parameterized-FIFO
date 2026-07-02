66b367c6d81b6c90030ee8f476bfeeff881598ee
saicharan24052 <saicharan24052@gmail.com>
2026-07-02 09:39:39 +0530

    Update fifo.sv

--- synchronous-FIFO/fifo.sv
+++ synchronous-FIFO/fifo.sv
@@ -8,6 +8,8 @@ module fifo #(
 	input	logic r_en,
 	output	logic full,
 	output	logic empty, 
+	//output	logic almost_full,
+	//output	logic almost_empty,
 	output	logic [DATA_WIDTH-1:0] data_out
 );
 
@@ -18,41 +20,38 @@ module fifo #(
 	logic loop_w, loop_r;
 	
 	
+	
 	always_ff @(posedge clk) begin
-		if(rst_n) begin
-			case ( {(r_en && !empty), (w_en && !full)}) 
-								
-				2'b01:begin
-			   		mem[w_ptr]	<= data_in;	
-			 	  	w_ptr		<= (w_ptr == (DEPTH-1))	? 0 : (w_ptr + 1); 
-			 	  	loop_w		<= (w_ptr == (DEPTH-1))	? (loop_w ^ 1'b1) : loop_w;
-				end 
-						
-				2'b10:begin
-			   		data_out	<= mem[r_ptr];	
-			 	  	r_ptr		<= (r_ptr == (DEPTH-1))	? 0 : (r_ptr + 1);
-			 	  	loop_r		<= (r_ptr == (DEPTH-1))	? (loop_r ^ 1'b1) : loop_r;                            
-				end
-
-				2'b11:begin
-			   		mem[w_ptr]	<= data_in;	
-			 	  	w_ptr		<= (w_ptr == (DEPTH-1))	? 0 : (w_ptr + 1); 
-			 	  	loop_w		<= (w_ptr == (DEPTH-1))	? (loop_w ^ 1'b1) : loop_w;
-			 	  	
-			   		data_out	<= mem[r_ptr];	
-			 	  	r_ptr		<= (r_ptr == (DEPTH-1))	? 0 : (r_ptr + 1);
-			 	  	loop_r		<= (r_ptr == (DEPTH-1))	? (loop_r ^ 1'b1) : loop_r;                            
-				end
-			endcase								                            
-			
-		end  
-		
-		else begin
-			w_ptr	<= 1'b0;
-			r_ptr	<= 1'b0;
-			loop_w	<= 'b0;
-			loop_r	<= 'b0;
+		if(!rst_n) begin
+			w_ptr	<= 1'b0;		
+			loop_w	<= 1'b0;
 		end
+		else begin		
+			if(w_en && !full) begin
+		   		mem[w_ptr]	<= data_in;	
+		   		w_ptr		<= (w_ptr == (DEPTH-1))	? 0 : (w_ptr + 1); 
+				loop_w		<= (w_ptr == (DEPTH-1))	? (loop_w ^ 1'b1) : loop_w;			
+	    	end
+	       	else
+	    	mem[w_ptr]	<= mem[w_ptr];
+	    end		
+	end
+	
+	
+	always_ff @(posedge clk) begin
+		if(!rst_n) begin
+	  		r_ptr	<= 1'b0;
+			loop_r	<= 1'b0;	
+		end
+		else begin
+			if(r_en && !empty) begin
+		  		data_out	<= mem[r_ptr];	
+	  	   		r_ptr		<= (r_ptr == (DEPTH-1))	? 0 : (r_ptr + 1);
+		   		loop_r		<= (r_ptr == (DEPTH-1))	? (loop_r ^ 1'b1) : loop_r;			
+	    	end
+	  		else
+	       	data_out	<= data_out;		
+		end	
 	end	
 
 assign full		= ((loop_w != loop_r) && (w_ptr == r_ptr)) ? 1 : 0; // hold your writing budd  
